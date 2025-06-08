ac74772fc02815de1e15bc493edb73c5cdb8bd25
lucasramosb <lucas.ramos@monocard.com.br>
2025-06-08 12:05:45 -0300

    feat: add config db and envoriment variables

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Client } from './client/client.model';
+import { ClientModule } from './client/client.module';
+import { PedidoModule } from './pedido/pedido.module';
+import { Pedido } from './pedido/pedido.model';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../../.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [Client, Pedido],
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false,
+    }),
+    ClientModule,
+    PedidoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
